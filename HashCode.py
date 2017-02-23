bc840097985a9bfa4f0a73e8e0a25a86e4391a85
Oliver Binns <mail@oliverbinns.info>
2017-02-23 19:24:25 +0000

    Finished data inputs from file

--- HashCode.py
+++ HashCode.py
@@ -2,16 +2,47 @@ import os
 
 
 class DataCentre(object):
-    def __init__(self, no_cache_servers, cache_server_size, no_request_descriptions, video_sizes, cache_connections):
+    def __init__(self, no_cache_servers, cache_server_size, no_request_descriptions, video_sizes, endpoints, requests):
         self.request_descriptions = no_request_descriptions
 
         self.cache_servers = []
         for i in range(no_cache_servers):
             self.cache_servers.append(CacheServer(cache_server_size))
 
-        videos = []
+        self.videos = []
         for i in range(len(video_sizes)):
-            videos.append(Video(i, video_sizes[i]))
+            self.videos.append(Video(i, video_sizes[i]))
+
+        self.end_points = []
+        for i in range(len(endpoints)):
+            endpoint_data = endpoints[i]
+            connections = endpoint_data["cache_connections"]
+
+            endpoint_obj = Endpoint(endpoint_data["latency"])
+            for j in range(len(connections)):
+                cache_id = connections[j]["id"]
+                endpoint_obj.add_connection(self.cache_servers[cache_id], connections[j]["latency"])
+
+        for i in range(requests):
+            request_data = requests[i]
+            endpoint = self.end_points[request_data["endpoint_id"]]
+            video = self.videos[request_data["video_id"]]
+
+            for j in range(request_data["no_requests"]):
+                endpoint.add_request(video)
+
+
+class Endpoint(object):
+    def __init__(self, latency):
+        self.latency = latency
+        self.cache_connections = {}
+        self.video_requests = []
+
+    def add_connection(self, cache, latency):
+        self.cache_connections[cache] = latency
+
+    def add_request(self, video):
+        self.video_requests.append(video)
 
 
 class Video(object):
@@ -40,18 +71,42 @@ directory = "data"
 # for file in os.listdir(directory):
 #    if file.endswith(".in"):
 f = open(os.path.join(directory, "kittens.in"), "r")
-data_center_config = f.readline()
+data_center_config = f.readline().split(" ")
 
 video_sizes = f.readline().split(" ")
 
 # End Point Video Requests
-cache_connections = []
-for i in range(int(data_center_config[1])):
-    cache_connections.append(f.readline().split())
+endpoints = []
+for i in range(int(data_center_config[1])): # For each of the end points..
+    endpoint_data = f.readline().split(" ")
+    endpoint_latency = endpoint_data[0]
+    no_cache_connections = endpoint_data[1]
+
+    cache_connections = []  # Fetch each cache
+    for j in range(int(no_cache_connections)):
+        cache_data = f.readline().split(" ")
+
+        cache_id = cache_data[0]
+        cache_latency = cache_data[1]
+
+        cache_connections.append({"id": int(cache_id), "latency": int(cache_latency)})
+    endpoints.append({"latency": int(endpoint_latency), "cache_connections": cache_connections})
+
+line = f.readline()
+requests = []
+while line != "":
+    request_data = line.split(" ")
+    requests.append({"video_id": int(request_data[0]), "endpoint_id": int(request_data[1]), "no_requests": int(request_data[2])})
+
+    line = f.readline()
+
+f.close()
 
 data_center = DataCentre(
-    int(data_center_config[2]), # Number of Request Descriptions
-    int(data_center_config[3]), # Number of Cache Servers
-    int(data_center_config[4]), # Size of Cache Server
-    video_sizes   # Size of Videos
+    int(data_center_config[2]),     # Number of Request Descriptions
+    int(data_center_config[3]),     # Number of Cache Servers
+    int(data_center_config[4]),     # Size of Cache Server
+    video_sizes,                    # Size of Videos
+    endpoints,
+    requests
 )
